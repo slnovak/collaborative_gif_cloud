ea7ab6535392a548b926eed64e8309b2dbde71ec
Stefan Novak <stefan@novak.io>
2016-01-08 15:03:18 -0800

    git logging in and logging out working

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,26 +1,2 @@
 class User::SessionsController < Devise::SessionsController
-  before_filter :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  protected
-
-  def configure_sign_in_params
-    devise_parameter_sanitizer.for(:sign_in) do |param|
-      u.permit :username, :email, :password, :password_confirmation    
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users, controllers: {
     sessions: 'user/sessions'
   }
+
+  root 'welcome#index' 
 end
