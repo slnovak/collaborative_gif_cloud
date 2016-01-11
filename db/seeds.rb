82d42c3338953f70dcee4541139cd0d92e3a1c51
Stefan Novak <stefan@novak.io>
2016-01-10 23:28:32 -0800

    fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ gifs = [{
     user: User.first
   },{
     title: "Nooppppeeee.",
-    description: "David tennant nods 'nope!'",
+    description: "David Tennant nods 'nope!'",
     tag_list: "giphy, doctor who",
     metadata: { source: "http://i.giphy.com/EVbEdEW3kuu0o.gif" },
     image: open("http://i.giphy.com/EVbEdEW3kuu0o.gif"),
